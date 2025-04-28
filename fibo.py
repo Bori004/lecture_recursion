22a152de2067d08b1479f34601da5cade0455dab
Borbala Udvaros <udvarosb1@gmail.com>
2025-04-28 13:24:41 +0200

    My fibo2

--- fibo.py
+++ fibo.py
@@ -1,15 +1,19 @@
-
+#There is a problem
 def recursive_ntb_fibo(n):
     if n <1:
-        return n
+        return 0
+    elif n == 1:
+        return 1
     else:
-        return n + recursive_ntb_fibo(n-1)
+        return recursive_ntb_fibo(n-2) + recursive_ntb_fibo(n-1)
 
 
 
 def main():
     n = int(input("Add number:"))
     print(recursive_ntb_fibo(n))
+    seq = [recursive_ntb_fibo(n) for num in range(n+1)]
+    print(seq)
 
 
 if __name__ == "__main__":
