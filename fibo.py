292ccfa9f8e25d42809461b112028cdf260a2b5f
Borbala Udvaros <udvarosb1@gmail.com>
2025-04-28 13:14:13 +0200

    My fibo

--- fibo.py
+++ fibo.py
@@ -1,6 +1,15 @@
 
+def recursive_ntb_fibo(n):
+    if n <1:
+        return n
+    else:
+        return n + recursive_ntb_fibo(n-1)
+
+
+
 def main():
-    pass
+    n = int(input("Add number:"))
+    print(recursive_ntb_fibo(n))
 
 
 if __name__ == "__main__":
